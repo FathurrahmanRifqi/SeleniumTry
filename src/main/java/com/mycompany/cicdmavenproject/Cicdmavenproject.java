a78834cd9e16eca14c53cc256f73dd41aae37645
FathurrahmanRifqi <fathurjr16@gmail.com>
2023-02-08 09:44:08 +0700

    first commit

---
+++ src/main/java/com/mycompany/cicdmavenproject/Cicdmavenproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cicdmavenproject;
+
+/**
+ *
+ * @author Pluto 09
+ */
+public class Cicdmavenproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
